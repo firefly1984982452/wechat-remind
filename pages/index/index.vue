bda35c865aa88a734d2be7407984dc892e3b775d
pengdan <1689586087@qq.com>
2020-05-26 17:59:32 +0800

    finsh

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,15 +1,22 @@
 <template>
-	<view class="content">
-		<view v-for="(item,index) in remindList" :key="index">
-			<text>{{item.content}}</text>
-			<text>{{item.time}}</text>
-		</view>
-		<view>
-			<view class="uni-input-wrapper">
-				<input class="uni-input" v-model="content" focus placeholder="请输入内容" />
+	<view class="page">
+		<view class="input">
+			<view class="line">
+				<text class="name">内容：</text>
+				<view class="uni-input-wrapper input-content">
+					<input class="uni-input" v-model="content" focus placeholder="请输入内容" />
+				</view>
 			</view>
-			<e-picker mode="dateTime"  @change="change">{{time||'选择日期'}}</e-picker>
-			<button type="primary" plain="true" @click="submit">确定添加提醒</button>
+			<view class="line">
+				<text class="name">时间：</text>
+				<e-picker mode="dateTime" class="input-time"  @change="change">{{time||'选择日期'}}</e-picker>
+			</view>
+			<button type="primary" class="button" plain="true" @click="submit" size="mini">确定</button>
+		</view>
+		<view v-for="(item,index) in remindList" :key="index" class="list">
+			<text class="content">{{item.content}}</text>
+			<text class="time">{{item.time}}</text>
+			<text class="del" @click="del(item,index)">删除</text>
 		</view>
 	</view>
 </template>
@@ -18,49 +25,152 @@
 	export default {
 		data() {
 			return {
-				remindList:[{content:'1',time:'2'}],
+				remindList:[],
 				content:'',
 				time: '',
+				remindKey:'remindKey'
 			}
 		},
 		onLoad() {
-
+			this.init()
 		},
 		methods: {
+			init(){
+				uni.getStorage({
+					key: this.remindKey,
+					success: (res) => {
+						console.log(res.data)
+						this.remindList = res.data;
+					},
+					fail: () => {
+						uni.setStorage({
+							key: this.remindKey,
+							data: [],
+							success: (res) => {
+								uni.showModal({
+									title: '存储数据成功',
+									content: ' ',
+									showCancel:false
+								})
+							},
+							fail: () => {
+								uni.showModal({
+									title: '储存数据失败!',
+									showCancel:false
+								})
+							}
+						})
+					}
+				})
+			},
             change(e) {
                 this.time = e
 			},
 			submit(){
-				console.log(this.content,this.time)
+				let item = {
+					content:this.content,
+					time:this.time
+				}
+				this.remindList.push(item);
+				uni.setStorage({
+					key: this.remindKey,
+					data: this.remindList,
+					success: (res) => {
+						uni.showToast({
+							title: "添加提醒成功",
+							duration: 1000
+						})
+						this.content = '';
+						this.time = '';
+					},
+					fail: () => {
+						uni.showModal({
+							title: '添加提醒失败!',
+							showCancel:false
+						})
+					}
+				})
+			},
+			del(item,index){
+				uni.showModal({
+					title: '提示',
+					content: '是否删除',
+					success: (res) => {
+						if (res.confirm) {
+							this.remindList.splice(index, 1);
+							uni.setStorage({
+								key: this.remindKey,
+								data: this.remindList,
+								success: (res) => {
+									uni.showToast({
+										title: "删除成功",
+										duration: 1000
+									})
+								},
+								fail: () => {
+									uni.showModal({
+										title: '删除失败!',
+										showCancel:false
+									})
+								}
+							})
+						} else if (res.cancel) {
+							console.log('用户点击取消');
+						}
+					}
+				});
 			}
 		},
 	}
 </script>
 
 <style>
-	.content {
+	.page{
+		padding: 10rpx;
+	}
+	.input {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		margin: 0 0 20rpx 0;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
+	.line{
 		display: flex;
-		justify-content: center;
+		flex-direction: row;
 	}
+	.name{
+		width: 150rpx;
+	}
+	.input-content{
+		width: 100%;
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	}
+	.input-time{
+		width: 100%;
+
+	}
+	.button{
+		width: 100%;
+	}
+	.list{
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	.content{
+		width: 300rpx;
+		text-overflow: ellipsis;
+		overflow: hidden;
+		white-space: nowrap;
+	}
+	.time{
+		width: 450rpx;
+		color: #666;
+	}
+	.del{
+		width: 100rpx;
+		height: 100%;
+		font-size: 10rpx;
+		color: #f00;
 	}
 </style>
